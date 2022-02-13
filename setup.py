72257e2ad0283683223e3a15778e415f499b13ce
Vasiliy <kopolokrbrgr@gmail.com>
2022-02-12 22:37:15 -0500

    fixed an error of .git being added as a python module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 
 def get_subdirectories(path):
-	return [item for item in os.listdir(path) if (os.path.isdir(item) and item != "build" and item != "datastructs.egg-info" and item != "dist")]
+	return [item for item in os.listdir(path) if os.path.isdir(item) and not (item in ("build", "datastructs.egg-info", "dist", ".git"))]
 
 def run_setup():
 	setup(name = os.getcwd().split('/')[-1], packages = get_subdirectories('.'))
