58b973409971e78e3f8d2cff47770a6f5023563f
Vasiliy <kopolokrbrgr@gmail.com>
2022-02-12 22:06:21 -0500

    create an __init__.py file for a module "stack"

---
+++ stack/__init__.py
@@ -0,0 +1,49 @@
+class Stack():
+        def __init__(self):
+            self._length = 0
+            self._head = None
+            
+        def __str__(self):
+            current = self._head
+            result = "Top ->"
+            while current:
+                result += f"[{current}]"
+                current = current.next
+            
+            result += "<- Bottom"
+            return result
+            
+        def __len__(self):
+            return self._length
+            
+        def push(self, value):
+            self._head = Stack_Element(value, self._head)
+            self._length += 1
+        
+        def isEmpty(self):
+            return self._length == 0
+            
+        def pop(self):
+            if not self.isEmpty():
+                value = self._head.value
+                self._head = self._head.next
+                self._length -= 1
+                return value
+            else:
+                raise RuntimeError("Stack is empty.")
+            
+        def peek(self):
+            if not self.isEmpty():
+                return self._head.value
+            else:
+                raise RuntimeError("Stack is empty.")
+        
+
+class Stack_Element():
+    def __init__(self, value, prev):
+        self.next = prev
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
